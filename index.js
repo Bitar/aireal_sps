3cb4d08e72a22b7c0972fbb26ac0060e2211cc99
Ayman Bitar <a.bitar@gmail.com>
2024-06-05 16:58:06 -0400

    Updated sps

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const WebSocket = require('ws');
 const axios = require('axios')
 const https = require('https');
 const fs = require('fs');
+require('dotenv').config();
 
-
-const privateKey = fs.readFileSync('/var/www/aireal-sps/certificates/client-key.pem');
-const certificate = fs.readFileSync('/var/www/aireal-sps/certificates/client-cert.pem');
+const privateKey = fs.readFileSync(process.env.APS_DOMAIN_PRIVATE_KEY);
+const certificate = fs.readFileSync(process.env.APS_DOMAIN_CERTIFICATE);
 
 const credentials = {
     key: privateKey,
     cert: certificate
 };
 
+const PORT = process.env.APS_PORT
+
 const httpsServer = https.createServer(credentials);
-httpsServer.listen(9090);
+httpsServer.listen(PORT);
 
 const wss = new WebSocket.Server({server: httpsServer});
-const API_URL = 'https://api.aireal.com'
-// const API_URL = 'https://staging.api.aireal.com'
-// const API_URL = 'http://api.aireal.test'
+const API_URL = process.env.APS_API_URL
 
 const AirealSPSType = {
     SERVER_INITIALIZED: 1,
@@ -27,17 +27,18 @@ const AirealSPSType = {
 
 const config = {
     headers: {
-        'X-API-KEY': '2k9SFijJEOk4NSswNtDo',
-        'X-API-SECRET': 'ojI4jfrbj0BuAFHKL2ODNHroeRfawRA7q5yHLjKvukwOyh8lgSXM2An6URyHO00Tlko1f6nlgCpZ7aJn',
+        'X-API-KEY': process.env.APS_X_API_KEY,
+        'X-API-SECRET': process.env.APS_X_API_SECRET,
         "Content-Type": "application/json",
     }
 }
+
 function setup(ws, serverSlug) {
     const formattedDate = new Date().toISOString();
     console.log('Connection Established');
     console.log('Issue Command for new Instance: ' + formattedDate)
 
-    axios.post(`${API_URL}/api/matchmaker`, {server: serverSlug}, config)
+    axios.post(`${API_URL}/api/matchmaker`, {application: serverSlug}, config)
         .then(response => {
             // handle success
             const data = response.data
@@ -88,9 +89,9 @@ function checkStreamerConnected(ws) {
 
 function notifyAirealStreamerConnected(ws, message) {
     const formattedDate = new Date().toISOString();
-    console.log('[' + formattedDate +  '] Notify Aireal Streamer is connected')
+    console.log('[' + formattedDate + '] Notify Aireal Streamer is connected')
 
-    axios.post(`${API_URL}/api/server/ready`, message,config)
+    axios.post(`${API_URL}/api/server/ready`, message, config)
         .then(response => {
             // handle success
             console.log('Aireal Notified')
@@ -102,7 +103,7 @@ function notifyAirealStreamerConnected(ws, message) {
                 // console.log(error.response.data);
                 // console.log(error.response.status);
                 // console.log(error.response.headers);
-                if(error.response.status === 400) {
+                if (error.response.status === 400) {
                     console.log('Server Instance doesnt belong to any of our servers. This should be terminated directly')
                 }
             } else if (error.request) {
@@ -125,12 +126,12 @@ wss.on('connection', function connection(ws) {
         try {
             let message = JSON.parse(data)
 
-            if(message.type === "streamerConnected") {
+            if (message.type === "streamerConnected") {
                 // update db and remove ps_check_scheduler
                 notifyAirealStreamerConnected(ws, message)
             }
 
-            if(message.type === 'setupInstance') {
+            if (message.type === 'setupInstance') {
                 setup(ws, message.server)
             }
 
